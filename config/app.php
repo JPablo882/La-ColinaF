c036101c4bd731a385385438e0da355e82466524
Juan Pablo <vacacarvalhojuanpablo1@gmail.com>
2026-02-28 20:06:31 -0400

    Registrar AdminLTE ServiceProvider manualmente

--- config/app.php
+++ config/app.php
@@ -156,20 +156,22 @@ return [
     */
 
     'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
 
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
+    /*
+     * Package Service Providers...
+     */
+    JeroenNoten\LaravelAdminLte\AdminLteServiceProvider::class,
 
+    /*
+     * Application Service Providers...
+     */
+    App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    // App\Providers\BroadcastServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+
+])->toArray(),
     /*
     |--------------------------------------------------------------------------
     | Class Aliases
